f583e72af7a896887939c5f680c620ea61b395ac
d_etteiu8383 <ysdr83@gmail.com>
2020-06-10 05:41:40 +0900

    remove: line_map, add: image2str_brightness
    - 線画アスキーアートの生成方法の見直し
    - 濃度アスキーアートの追加

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ output_path = "output_001.png"
 
 img = AA.open_image(input_path)
 img = AA.paste_img(img)
-threshold = AA.gray2line(AA.image2gray(img))
-AA.images2str_lines(threshold, 40)
+gray = AA.image2gray(img)
+AA.image2str_brightness(gray, 80, 2)
